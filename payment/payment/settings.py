68bded028ffaffd4e889cf4192bd6abb481bd9d4
bspark2318 <bspark2318@gmail.com>
2023-07-10 22:31:00 -0400

    commiting base work - far from done

--- payment/payment/settings.py
+++ payment/payment/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8ncenuc2&3py7gti(0rqtriq34v$$n!!wp266*s(+ix1abhmee'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+## Stripe API keys 
+# SECURITY WARNING: keep the API key used in production secret!
+STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
+STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'payment_app',
 ]
 
 MIDDLEWARE = [
